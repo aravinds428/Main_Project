3d5cd30251e3535cfa47ea69210431968249f4a9
aravind <aravinds428@gmail.com>
2018-01-07 11:40:56 +0530

    Modified sent.py

--- sent.py
+++ sent.py
@@ -96,7 +96,7 @@ def main():
     # creating object of TwitterClient Class
     api = TwitterClient()
     # calling function to get tweets
-    tweets = api.get_tweets(query='Donald Trump', count=200)
+    tweets = api.get_tweets(query='Donald Trump lang:en', count=200) # lang:en to get only english tweets
 
     # picking positive tweets from tweets
     ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
